85386b1bb486ababff82cd68d69f3bdb6211cf76
guomengtao <rinuo@qq.com>
2020-02-22 14:26:22 +0800

    test

--- webhook.php
+++ webhook.php
@@ -6,7 +6,9 @@ namespace think;
 echo "666";
 
 echo shell_exec('ls -la');
-
+echo shell_exec('ll');
+echo shell_exec('git clone git@gitee.com:rinuo/phpmanual.git');
+echo shell_exec('ll');
 class webhook
 {
 

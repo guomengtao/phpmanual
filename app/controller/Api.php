4a88cf73a9b05968d7b992b44e8781855588400d
guomengtao <rinuo@qq.com>
2020-03-02 03:38:16 +0800

    测试

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -1,10 +1,10 @@
 <?php
 
-
 namespace app\controller;
 
 use app\BaseController;
 use app\model\Manual;
+use think\App;
 
 class Api extends BaseController
 {
@@ -12,5 +12,25 @@ class Api extends BaseController
     {
         $list = Manual::order('id', 'asc')->select();
         echo $list->toJson();
+    }
+
+    public function tom()
+    {
+
+    }
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+    }
+
+    public function tomy()
+    {
+
+    }
+
+    public static function game()
+    {
+
     }
 }
